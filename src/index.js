bb617d3e07e974d623b66b0e5290807f6fc693e8
dasuchka <70776259+dasuchka@users.noreply.github.com>
2020-10-22 11:05:14 +0300

    refactor: correct mistakes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 module.exports = function towelSort(matrix){
     let line_matrix=[];
     for (let i=0; i<=matrix.length-1;i++){
-        for (let j=0; j<=matrix[i].length-1; j++){
+        if (i%2===0){
+            for (let j=0; j<=matrix[i].length-1; j++){
             line_matrix.push(matrix[i][j]);
-        }
+            }
+        }else{for (let j=matrix[i].length-1; j>=0; j--){
+            line_matrix.push(matrix[i][j]);
+            }}
     }
     return line_matrix;
 }
